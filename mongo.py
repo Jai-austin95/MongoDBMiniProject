db88bce7caf1ceccb471511672f3dd720e6ae78a
Jai-austin95 <jai.austin95@gmail.com>
2020-05-25 12:59:46 +0000

    initial commit

--- mongo.py
+++ mongo.py
@@ -2,17 +2,17 @@ import pymongo
 import os
 
 MONGODB_URI = os.getenv("MONGO_URI")
-DBS_NAME = "myTestDB"
+DBS_NAME = "mytestdb"
 COLLECTION_NAME = "myFirstMDB"
 
 def mongo_connect(url):
     try:
         conn = pymongo.MongoClient(url)
-        print("mongo is connected.")
+        print("Mongo is connected!")
         return conn
     except pymongo.errors.ConnectionFailure as e:
-        print("could not connect to mongoDB: %s") % e
-
+        print("Could not connect to MongoDB: %s") % e
+        
 conn = mongo_connect(MONGODB_URI)
 
 coll = conn[DBS_NAME][COLLECTION_NAME]
